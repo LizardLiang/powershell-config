5fbc0e9dfc85e3fc9724c27908beff83cc940de9
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-04-18 13:25:48 +0800

    feat: add alias for git usage

--- user_profile.ps1
+++ user_profile.ps1
@@ -28,12 +28,43 @@ Import-Module ZLocation
 Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias g git
-Set-Alias st 'git status'
-Set-Alias gcz 'git cz'
+Set-Alias gcz 'C:\Program Files\Git\cmd\git.exe cz'
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias cc 'Set-Clipboard'
+
+function git-log-graph  { g log --graph --decorate --oneline }
+Set-Alias -Name glg -Value git-log-graph
+
+function git-diff { git diff }
+Set-Alias -Name gd -Value git-diff
+
+function git-checkout { git checkout }
+Set-Alias -Name gco -Value git-checkout
+
+function git-fetch { git fetch }
+Set-Alias -Name gf -Value git-fetch
+
+function git-pull { git pull $args }
+Set-Alias -Name gpl -Value git-pull
+
+del alias:gp -Force
+function git-push { git push $args }
+Set-Alias -Name gp -Value git-push
+
+function git-branch { git branch }
+Set-Alias -Name gb -Value git-branch
+
+function git-status { git status }
+Set-Alias -Name gs -Value git-status
+
+del alias:gc -Force
+function git-commit { git commit }
+Set-Alias -Name gc -Value git-commit
+
+function back-dir { cd .. }
+Set-Alias -Name .. -Value back-dir
 
 # Utilities
 function which ($command) {
